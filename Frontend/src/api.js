1967674bf16453b5f0565434bcc811114939c3cb
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-08-11 00:02:12 +0530

    first

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,10 @@
+
+
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL
+});
 
 // Attach JWT from localStorage for all requests
 API.interceptors.request.use((req) => {
