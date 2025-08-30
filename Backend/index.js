cdbf5fb4da3a375f552232cce3dcceed0b97bdec
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-08-30 14:13:23 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ database();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173' ,  "https://shivam23.vercel.app/"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
